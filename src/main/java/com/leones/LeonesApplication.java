d27b5d0a3f60ba8548e12054eb963471e1051b93
ALF-2 <ALF-2@Datsun240z>
2025-10-29 00:14:29 -0600

    Commit 1 (Preparacion de ambiente web) Jose Alejandro Lafuente romero

---
+++ src/main/java/com/leones/LeonesApplication.java
@@ -0,0 +1,13 @@
+package com.leones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeonesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leones/LeonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
